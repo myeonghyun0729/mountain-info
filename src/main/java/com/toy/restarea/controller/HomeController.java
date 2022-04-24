14986a86dc5cf1384ebebf5ad78163e9292b918f
myeonghyun0729 <audgus729@daum.net>
2022-04-24 18:26:25 +0900

    jsp 설정 적용

--- src/main/java/com/toy/restarea/controller/HomeController.java
+++ src/main/java/com/toy/restarea/controller/HomeController.java
@@ -2,29 +2,26 @@ package com.toy.restarea.controller;
 
 import com.toy.restarea.service.TestService;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.Map;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired
     private TestService testService;
 
     @GetMapping
-    public String home() {
-        return "Hello Spring Boot";
-    }
-
-    @GetMapping("/test/create")
-    public Map createTest() {
-        Long id = testService.insertTest(RandomStringUtils.randomAlphabetic(4));
-        if (id != null && id.intValue() <= 0) {
-            return Map.of("result", "false");
-        }
-        return Map.of("result", "success");
+    public String home(Model model) {
+        String today = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+        model.addAttribute("today", today);
+        return "home";
     }
 }
--- src/main/java/com/toy/restarea/controller/admin/AdminHomeController.java
+++ src/main/java/com/toy/restarea/controller/admin/AdminHomeController.java
@@ -1,5 +1,8 @@
 package com.toy.restarea.controller.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminHomeController {
 
+    @Autowired
+    private static Logger logger = LoggerFactory.getLogger(AdminHomeController.class);
+
     @GetMapping
     public String home() {
-        return "index.html";
+        logger.info("home!");
+        return "admin/home";
     }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        logger.info("dashboard!");
+        return "dashboard";
+    }
+
 }
